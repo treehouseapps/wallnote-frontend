0e30b711bdb9231f7122db7da3e6094a9b51163d
treehouseapps <109871372+treehouseapps@users.noreply.github.com>
2024-10-15 22:21:29 +0300

    Update Item.jsx

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 import Button from "./Button";
 const Templete = (props) => {
-    const link = "http://localhost:4000/delete/"
-    const page = "http://localhost:3000/modify/"
+    const link = "https://wallnote-backend.vercel.app/delete/"
+    const page = "https://wallnote-backend.vercel.app/modify/"
     return (
         <div className="border rotate">
             <div className="item">
